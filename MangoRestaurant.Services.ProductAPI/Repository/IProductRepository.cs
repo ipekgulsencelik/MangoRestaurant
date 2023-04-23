b79a80dab7f1a409628c6bc3655f5c171d465e47
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-23 19:19:21 +0300

    Product Repository Interface

---
+++ MangoRestaurant.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using MangoRestaurant.Services.ProductAPI.Models.DTO;
+
+namespace MangoRestaurant.Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDTO>> GetProducts();
+        Task<ProductDTO> GetProductByID(int productID);
+        Task<ProductDTO> CreateUpdateProduct(ProductDTO productDTO);
+        Task<bool> DeleteProduct(int productID);
+    }
+}
