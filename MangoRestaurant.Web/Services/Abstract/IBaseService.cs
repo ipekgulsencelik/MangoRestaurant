6b03290c155ff084067a8b62f8345a94568f3098
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-23 22:00:46 +0300

    Base Service Interface and API Request Model

---
+++ MangoRestaurant.Web/Services/Abstract/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using MangoRestaurant.WebUI.Models;
+
+namespace MangoRestaurant.WebUI.Services.Abstract
+{
+    public interface IBaseService : IDisposable
+    {
+        ResponseDTO responseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
