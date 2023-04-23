97737f0bc1128af624aad0a38d4a07a474b5cec1
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-23 19:06:07 +0300

    Add project files.

---
+++ MangoRestaurant.Services.ProductAPI/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MangoRestaurant.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MangoRestaurant.Services.ProductAPI.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ MangoRestaurant.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MangoRestaurant.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
